e425c832f5f50d92060fcd5e76a6d19b80941065
nlebang <nlebang@gmail.com>
2021-10-10 20:45:46 -0400

    Update base_model.py
    added comments

--- models/base_model.py
+++ models/base_model.py
@@ -46,15 +46,19 @@ class StockBlockLayer(nn.Module):
     def spe_seq_cell(self, input):
         batch_size, k, input_channel, node_cnt, time_step = input.size()
         input = input.view(batch_size, -1, node_cnt, time_step)
+        #discrete fourier transform
         ffted = torch.rfft(input, 1, onesided=False)
+        #1DConv
         real = ffted[..., 0].permute(0, 2, 1, 3).contiguous().reshape(batch_size, node_cnt, -1)
         img = ffted[..., 1].permute(0, 2, 1, 3).contiguous().reshape(batch_size, node_cnt, -1)
+        #GLU
         for i in range(3):
             real = self.GLUs[i * 2](real)
             img = self.GLUs[2 * i + 1](img)
         real = real.reshape(batch_size, node_cnt, 4, -1).permute(0, 2, 1, 3).contiguous()
         img = img.reshape(batch_size, node_cnt, 4, -1).permute(0, 2, 1, 3).contiguous()
         time_step_as_inner = torch.cat([real.unsqueeze(-1), img.unsqueeze(-1)], dim=-1)
+        #inverse DFT
         iffted = torch.irfft(time_step_as_inner, 1, onesided=False)
         return iffted
 
